1611b807ce6f81083ab4a953bb664443ed06c0d0
132435ty <Kz89267908477@gmail.com>
2024-03-13 20:54:33 +0300

    123

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-def generate(a, w, h):
-    return [[a] * w] * h
-a = 2
-w = 5
-h = 6
-res = generate(a, w, h)
-print(res)
+list = [
+    "<li>Hello</li>",
+    "<li>I</li>",
+    "<li>Am</li>",
+    "<li>A</li>",
+    "<li>Robot</li>",
+    "<li>Привет</li>",
+    "<li>Это</li>",
+    "<li>статья</li>",
+    "<li>пуста</li>",
+    "<li>извини</li>",
+    "<li>пожалуста</li>"
+]
+
+cleanedlist = [x.replace("<li>", "").replace("</li>", "")
+               for x in list]
+
+print(cleanedlist)
