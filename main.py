077fd901a20d89a2002dc046e45066c509002007
132435ty <Kz89267908477@gmail.com>
2024-03-13 21:19:36 +0300

    123

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-list = [
-    "<li>Hello</li>",
-    "<li>I</li>",
-    "<li>Am</li>",
-    "<li>A</li>",
-    "<li>Robot</li>",
-    "<li>Привет</li>",
-    "<li>Это</li>",
-    "<li>статья</li>",
-    "<li>пуста</li>",
-    "<li>извини</li>",
-    "<li>пожалуста</li>"
-]
+list = '''
+<li>
+    Hello
+</li>
+<li>
+	I
+</li>
+<li>
+	Am
+</li>
+<li>
+	A
+</li>
+<li>
+	Robot
+</li>
+<li>
+	Привет
+</li>
+<li>
+	Это
+</li>
+<li>
+	статья
+</li>
+<li>
+	пуста
+</li>
+<li>
+	извини
+</li>
+<li>
+	пожалуста
+</li>
+'''
 
-cleanedlist = [x.replace("<li>", "").replace("</li>", "")
-               for x in list]
-
-print(cleanedlist)
+cleanedlist = ' '.join(map(lambda x: x, list.split('</li>')))
+cleanedlist1 = ' '.join(lambda x: x, cleanedlist.split('<li>')))
+print(cleanedlist1)
